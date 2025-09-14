6c130830d548fafba90b9e4d12ff5f81c16d3da7
mavmodz <gaurav.chauhan@techglide.in>
2025-09-14 17:25:07 +0530

    updated new origin

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(morgan('combined'));
 const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:5500',
-  'https://mavmodz.github.io/valeti-new-home/index.html',
+  'https://valeti-new-home.vercel.app',
   'https://valeti-kappa.vercel.app',
 ];
 
