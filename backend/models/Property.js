93f0ec2039869621adfaea4ea6976721d6c5226e
mavmodz <gaurav.chauhan@techglide.in>
2026-03-17 20:44:55 +0530

    category added for back to back

--- backend/models/Property.js
+++ backend/models/Property.js
@@ -19,7 +19,7 @@ const propertySchema = new mongoose.Schema({
   },
   category: {
     type: String,
-    enum: ['Single Story', 'Double Story'],
+    enum: ['Single Story', 'Double Story', 'Back To Back'],
     required: [true, 'Property category is required']
   },
   propertyFor: {
